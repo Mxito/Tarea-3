f4c3cb49daca658c07a23b8a35dc94579cd43ae5
Max <maxilopezbec@gmail.com>
2022-11-07 16:44:15 -0300

    clase ventana y panel creadas

---
+++ Tarea3/src/tarea3/PanelPrincipal.java
@@ -0,0 +1,10 @@
+package tarea3;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class PanelPrincipal extends JPanel {
+    public PanelPrincipal(){
+        this.setBackground(Color.white);
+        
+    }
+}
---
+++ Tarea3/src/tarea3/Ventana.java
@@ -0,0 +1,20 @@
+package tarea3;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ventana extends JFrame{
+    public PanelPrincipal panel;
+    public Ventana(){
+        this.setSize(700, 700);
+        setTitle("Titulo");
+
+        this.add(new PanelPrincipal());
+    
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+}
+
